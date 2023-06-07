518ebf35b6155d25d8075cababa0a146b70e6f72
Kostya119 <Kostya111119@gmail.com>
2023-06-07 20:40:05 +0200

    ShuttleNumber

---
+++ Src/ShuttleNumber.java
@@ -0,0 +1,12 @@
+public class ShuttleNumber {
+    public static void main(String[] args) {
+        int shuttleNumber = 1;
+        for (int i = 0; i < 100; i++) {
+            if (shuttleNumber % 10 == 4 || shuttleNumber % 10 == 9) {
+                shuttleNumber++;
+            }
+            System.out.println(shuttleNumber);
+            shuttleNumber++;
+        }
+    }
+}
